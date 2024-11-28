83fa2b726c04b608d2f726dc84cf6cac7f5ac34a
Rachael Horsman <149065458+rach-horsman@users.noreply.github.com>
2024-11-28 13:30:41 +0000

    Update gps_print.py

--- gps_print.py
+++ gps_print.py
@@ -8,7 +8,7 @@ while True:
 	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
 	dataout = pynmea2.NMEAStreamReader()
 	newdata=ser.readline()
-        n_data = newdata.decode('latin-1')
+	n_data = newdata.decode('latin-1')
 
 	if n_data[0:6] == "$GPRMC":
 		newmsg=pynmea2.parse(n_data)
