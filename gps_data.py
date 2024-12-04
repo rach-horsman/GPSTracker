2bbaac93f61b2515e12df2c5418f35bcd92e743b
Rachael Horsman <149065458+rach-horsman@users.noreply.github.com>
2024-12-04 20:41:50 +0300

    Update gps_data.py

--- gps_data.py
+++ gps_data.py
@@ -1,1 +1,12 @@
-b
+import serial
+import time
+import string
+import pynmea2
+
+while True:
+	port="/dev/ttyAMA0"
+	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+	dataout = pynmea2.NMEAStreamReader()
+	newdata=ser.readline()
+	n_data = newdata.decode('latin-1')
+	print(n_data)
